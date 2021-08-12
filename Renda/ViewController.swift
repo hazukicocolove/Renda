d1e1f762e2ef2514886f1a6cbc9cacc9130df30e
HazukiTamura <pretty11_13@yahoo.co.jp>
2021-08-13 01:04:51 +0900

    Initial Commit

---
+++ Renda/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Renda
+//
+//  Created by Hazuki Tamura on 2021/08/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
